661dc7ae608b52151c6f6c25da486744cda44fd4
Yaroslav Lashchenko <yariklashchenko@gmail.com>
2019-05-16 21:48:44 +0200

    updates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Route, Link } from "react-router-dom";
 import { Helmet } from "react-helmet";
 import Login from "./components/Login";
 import Logout from "./components/Logout";
@@ -23,34 +23,32 @@ function App() {
   }, []);
 
   return (
-    <Router basename={process.env.PUBLIC_URL}>
-      <div className="App">
-        {!userToken ? null : (
-          <header className="nav-header">
-            <Helmet>
-              <title>AmazingBox</title>
-            </Helmet>
-            <Link className="home-icon material-icons" to="/">
-              home
-            </Link>
-            <Link className="favorites" to="/favorites">
-              <i className="favorites-icon material-icons">star</i>{" "}
-              <span className="favorites-text">Favorites</span>
-            </Link>
-            <Link className="search-icon material-icons" to="/search">
-              search
-            </Link>
-            <Logout />
-          </header>
-        )}
-        <Route exact path="/" component={Home} />
-        <Route path="/home" component={Home} />
-        <Route path="/favorites" component={Favorites} />
-        <Route path="/search" component={SearchView} />
-        <Route path="/auth" component={Login} />
-        <Route path="/auth-done" component={LoginDone} />
-      </div>
-    </Router>
+    <div className="App">
+      {!userToken ? null : (
+        <header className="nav-header">
+          <Helmet>
+            <title>AmazingBox</title>
+          </Helmet>
+          <Link className="home-icon material-icons" to="/">
+            home
+          </Link>
+          <Link className="favorites" to="/favorites">
+            <i className="favorites-icon material-icons">star</i>{" "}
+            <span className="favorites-text">Favorites</span>
+          </Link>
+          <Link className="search-icon material-icons" to="/search">
+            search
+          </Link>
+          <Logout />
+        </header>
+      )}
+      <Route exact path="/" component={Home} />
+      <Route path="/home" component={Home} />
+      <Route path="/favorites" component={Favorites} />
+      <Route path="/search" component={SearchView} />
+      <Route path="/auth" component={Login} />
+      <Route path="/auth-done" component={LoginDone} />
+    </div>
   );
 }
 
