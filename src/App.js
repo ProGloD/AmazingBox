ceda37d13499add7bcd04a31077032c482baa794
Yaroslav Lashchenko <yariklashchenko@gmail.com>
2019-05-16 20:28:24 +0200

    updates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { HashRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import { Helmet } from "react-helmet";
 import Login from "./components/Login";
 import Logout from "./components/Logout";
