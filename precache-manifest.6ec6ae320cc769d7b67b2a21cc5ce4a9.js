7802d5fe011a993805bea7b2b5ac1cd0d924f3a5
Yaroslav Lashchenko <yariklashchenko@gmail.com>
2019-05-16 20:05:47 +0200

    Updates

--- precache-manifest.6ec6ae320cc769d7b67b2a21cc5ce4a9.js
+++ precache-manifest.6ec6ae320cc769d7b67b2a21cc5ce4a9.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "481039beafde92e8b1664beb30f6b80c",
+    "revision": "69e86325db1a84e785a521da254733ad",
     "url": "/AmazingBox/index.html"
   },
   {
-    "revision": "f59b17ea89983e801980",
+    "revision": "358d71977ecf0f79795e",
     "url": "/AmazingBox/static/css/main.3e490b9b.chunk.css"
   },
   {
-    "revision": "221653fd9b6db5739cf0",
-    "url": "/AmazingBox/static/js/2.94e3d3ea.chunk.js"
+    "revision": "981b4c06fe243b557a53",
+    "url": "/AmazingBox/static/js/2.4f374566.chunk.js"
   },
   {
-    "revision": "f59b17ea89983e801980",
-    "url": "/AmazingBox/static/js/main.0a56c6ca.chunk.js"
+    "revision": "358d71977ecf0f79795e",
+    "url": "/AmazingBox/static/js/main.d2e96c8f.chunk.js"
   },
   {
     "revision": "176ea492c1c3cfcb5feb",
